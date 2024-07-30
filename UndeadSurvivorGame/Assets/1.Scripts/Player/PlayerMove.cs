2531a460db3d264c84d01eb0ccf66de2a086a32a
정현우 <jhw504166@gmail.com>
2024-07-30 23:17:31 +0900

    24.07.30 : Add : Player Animation and Override

--- UndeadSurvivorGame/Assets/1.Scripts/Player/PlayerMove.cs
+++ UndeadSurvivorGame/Assets/1.Scripts/Player/PlayerMove.cs
@@ -10,10 +10,14 @@ public class PlayerMove : MonoBehaviour
     Vector2 inputVec;
 
     Rigidbody2D rb;
+    SpriteRenderer sprite;
+    Animator animator;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     void Start()
@@ -31,5 +35,15 @@ public class PlayerMove : MonoBehaviour
     void OnMove(InputValue value)
     {
         inputVec = value.Get<Vector2>();
+    }
+
+    private void LateUpdate()
+    {
+        animator.SetFloat("Speed", inputVec.magnitude);
+
+        if (inputVec.x != 0)
+        {
+            sprite.flipX = inputVec.x < 0;
+        }
     }
 }
