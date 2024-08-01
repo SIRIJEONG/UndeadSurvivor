37a5542b63a164b35ab613423730a532bd03367b
정현우 <jhw504166@gmail.com>
2024-08-01 14:34:20 +0900

    24.08.01 : Add : Enemy Script / Enemy Move

---
+++ UndeadSurvivorGame/Assets/1.Scripts/Monster/Enemy.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+
+    public Rigidbody2D target;
+
+    bool isLive = true;
+
+    Rigidbody2D rb;
+
+    SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        if(!isLive)
+            return;
+        
+        
+        Vector2 dirVec = target.position - rb.position;
+        Vector2 nextVec = dirVec.normalized * speed * Time.fixedDeltaTime;
+
+        rb.MovePosition(rb.position + nextVec);
+
+        rb.velocity = Vector2.zero;
+    }
+
+    private void LateUpdate()
+    {
+        if (!isLive)
+            return;
+
+        spriteRenderer.flipX = target.position.x < rb.position.x;
+    }
+}
